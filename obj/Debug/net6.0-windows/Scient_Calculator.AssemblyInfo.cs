d1027220d5f70ecb9a1ef7a46fa97d28c9e9193c
Zerihun Zefine <www.zerihunzefine2022@gmail.com>
2024-03-21 01:24:56 +0300

    a bit change

--- obj/Debug/net6.0-windows/Scient_Calculator.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/Scient_Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scient_Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6e683dad83d50d71c9efb32b214443c97b72df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scient_Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scient_Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
